b92fb208f4179acbea17d78c5329c9f970a6b4ca
Xurshida <121751528+Xurshida6569@users.noreply.github.com>
2024-08-15 13:52:01 +0500

    Add files via upload

---
+++ src/components/Calendar/Calendar.jsx
@@ -0,0 +1,17 @@
+
+const Calendar = () => {
+  return (
+    <div className="w-[241px] h-[323px] border border-black">
+      <div className="container mt-[8px] mb-[23px] mx-[12px]">
+        <div className="flex justify-between">
+        <h1 className="title">Reschedule Product</h1>
+        <div className="closeBtn">
+            icon
+        </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Calendar
